acc5a0c17a4c3245570e8a75726cc1f29bc78a80
Anil Kumar <kumarsark69@gmail.com>
2023-02-02 17:58:29 +0530

    Add button click listerner with text view

--- app/src/main/java/com/example/firebasetes/MainActivity.java
+++ app/src/main/java/com/example/firebasetes/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.firebasetes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button) findViewById(R.id.button);
+        txt=(TextView) findViewById(R.id.textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),txt.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
