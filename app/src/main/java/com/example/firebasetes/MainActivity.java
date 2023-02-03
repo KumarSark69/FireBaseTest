5e9ce485eaa8a1b925295e1d08255716a983e83a
Anil Kumar <kumarsark69@gmail.com>
2023-02-03 09:26:43 +0530

    add edit view

--- app/src/main/java/com/example/firebasetes/MainActivity.java
+++ app/src/main/java/com/example/firebasetes/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.firebasetes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,16 +15,18 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
     TextView txt;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=(Button) findViewById(R.id.button);
         txt=(TextView) findViewById(R.id.textView2);
+        editText=(EditText) findViewById(R.id.editTextTextPersonName);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),txt.getText().toString(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Hello "+editText.getText().toString(),Toast.LENGTH_SHORT).show();
             }
         });
     }
